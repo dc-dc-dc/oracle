5a33cc3ef799f9805793791766c02707b50531e6
Diogo Da Cruz <dgcruz983@gmail.com>
2023-06-23 15:36:18 -0400

    cuda build working on windows

--- extra/cuda.c
+++ extra/cuda.c
@@ -1,8 +1,18 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void main() {
+#define ERROR_WRAPPER(x) do { cudaError_t err = x; if (err != cudaSuccess) { printf("CUDA error: %s\n", cudaGetErrorString(err)); exit(1); } } while (0)
+
+int main(int argc, char **argv) {
     int deviceCount;
-    cudaGetDeviceCount(&deviceCount);
+    ERROR_WRAPPER(cudaGetDeviceCount(&deviceCount));
     printf("Device count: %d\n", deviceCount);
+    for(size_t i = 0; i < deviceCount; i++) {
+        struct cudaDeviceProp prop;
+        ERROR_WRAPPER(cudaGetDeviceProperties(&prop, i));
+        printf("Device %zu \n", i);
+        printf("\tName: %s\n", prop.name);
+    }
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,11 @@
+.PHONY: clean metal cuda_windows
+
 metal:
-	clang -lobjc -framework Metal -framework CoreGraphics -framework Foundation extra/metal.m -o metal-info && ./metal-info 
-	rm metal-info
+	clang -lobjc -framework Metal -framework CoreGraphics -framework Foundation extra/metal.m -o ./out/metal-info && ./metal-info 
+
+cuda_windows:
+	clang ./extra/cuda.c -o ./out/cuda.exe -I"${CUDA_PATH}\include" -L"${CUDA_PATH}\lib\x64" -lcuda -lcudart
+	./out/cuda.exe
+
+clean:
+	rm -rf ./out/*
